fcf8ffe9e1ecd16cfcc2015c5d3a766e9b90fbd7
Thokozile Soko <thokozile.soko@yahoo.com>
2021-01-18 11:24:19 -0500

    I've created a fake readme to see how the readme displays on Github.

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,19 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
+const asyncWrite = util.promisify(fs.writeFile);
+
 // const badmath = require('badmath');
 
+const generateMD = (data) =>
+`# ${data.title} 
+## ${data.description}
+### ${data.installation}
+#### ${data.usage}
+##### ${data.deployed}
+###### ${data.tests}` ;
 // TODO: Create an array of questions for user input
 // const questions = [];
 inquirer
@@ -64,22 +75,28 @@ inquirer
       },
   ])
   .then((data) => {
-  const filename = `${data.title}.md`;
+  const fileName = `${data.title}.md`;
+//       fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+//     err ? console.log(err) : console.log('Readme complete')
+//   );
+  asyncWrite(fileName, generateMD(data), (err) =>
+  err ? console.log(err) : console.log('Success!')
+  )
 
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.log(err) : console.log('Success!')
-  );
 });
 
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
-//     const fileName = `${data.title}.md`;
-//     fs.writeFile(fileName, data), (err) =>()
-//     );
+// //     fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+// //     err ? console.log(err) : console.log('Readme complete')
+// //   );
+// //   asyncWrite(fileName, generateMD(data), (err) =>
+// //   err ? console.log(err) : console.log('Success!')
+// //   )
 
 // }
-
+// writeToFile();
 // // TODO: Create a function to initialize app
 // function init() {}
 
